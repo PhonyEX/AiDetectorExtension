d45b0b63ecaa56bd43b83d87dd94ed688209ece8
PhonyEX <116227832+PhonyEX@users.noreply.github.com>
2023-06-30 13:32:22 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,28 @@
 const apiKey = "QY21KK1KOQ8Z098CS63OCMZLFHLL1ADY";
 
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "detectText",
+    title: "Detect AI",
+    contexts: ["selection"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === "detectText") {
+    const text = info.selectionText;
+    openExtensionAndDetectText(text);
+  }
+});
+
+const openExtensionAndDetectText = (text) => {
+  chrome.runtime.sendMessage({ event: "openExtension" });
+
+  setTimeout(() => {
+    chrome.runtime.sendMessage({ event: "highlightedText", text });
+  }, 500);
+};
+
 chrome.runtime.onMessage.addListener(async (data) => {
   try {
     if (data.event === "detectAI" && data.prefs.text.trim("") !== "") {

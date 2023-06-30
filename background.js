3d85310a37a2ec942607fdc0485f1618b7aab8be
PhonyEX <116227832+PhonyEX@users.noreply.github.com>
2023-06-30 13:29:00 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,32 +1,8 @@
 const apiKey = "QY21KK1KOQ8Z098CS63OCMZLFHLL1ADY";
 
-/* chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'detectAI') {
-    const text = request.text;
-
-    fetch('https://api.sapling.ai/api/v1/aidetect', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({ text }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        sendResponse({ result });
-      })
-      .catch((error) => {
-        sendResponse({ error: error.message });
-      });
-
-    return true;
-  }
-}); */
-
 chrome.runtime.onMessage.addListener(async (data) => {
   try {
-    if (data.event == "detectAI" && data.prefs.text.trim("") != "") {
+    if (data.event === "detectAI" && data.prefs.text.trim("") !== "") {
       const input = {
         key: apiKey,
         text: data.prefs.text,
